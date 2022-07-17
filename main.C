1d87bee3a731438155f545a07fd5c640be70683c
Naveed <98022575+SNUcoder@users.noreply.github.com>
2022-07-17 18:54:07 +0530

    Update main.C

--- main.C
+++ main.C
@@ -1,5 +1,5 @@
-#include <E:\Naveed\Programming\C experiments\libs of own\doit.h>
+#include <E:\Naveed\Programming\C experiments\libs of own\doit.h> //Include the library which we created
 
-int main(){
-	print("Hello! I am ATeX your friendly assistant");
+int main(){ //main function
+	print("Hello! I am ATeX your friendly assistant"); // Print the line
 }
