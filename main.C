cca1b328d30db60304509808118ac5d620bf7d24
Naveed <98022575+SNUcoder@users.noreply.github.com>
2022-07-17 18:50:44 +0530

    Update main.C

--- main.C
+++ main.C
@@ -1,9 +1,5 @@
 #include <E:\Naveed\Programming\C experiments\libs of own\doit.h>
 
-int read(char * b[]){
-	scanf("%s", &b);
-}
-
 int main(){
 	print("Hello! I am ATeX your friendly assistant");
 }
